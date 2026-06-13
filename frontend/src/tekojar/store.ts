92ef4682797c47744c9dfd43bb2f53f038573773
helmimuzkr <mhelmimuzakir@gmail.com>
2026-06-13 17:26:44 +0700

    feat(tekojar): add clear logs and add delete 20% log feature when the size exceeds 1MB

--- frontend/src/tekojar/store.ts
+++ frontend/src/tekojar/store.ts
@@ -3,14 +3,19 @@ import type { Service, Log } from "./type";
 import { EventsOn } from "../../wailsjs/runtime/runtime";
 import { GetAll, Start, Stop, Get as GetById } from "../../wailsjs/go/app/TekojarApp";
 
-// --- State ---
+// CONST
+const MAX_BYTES = 1 * 1024 * 1024; // 1MB
+const TRIM_RATIO = 0.2; // if logs hit the max_bytes limit, drop oldest 20%
+const logBufferSizeByService: Record<string, number> = {};
+
+// STATE 
 export const services = writable<Service[]>([]);
 export const selectedServiceId = writable<string | null>(null);
 export const serviceLogs = writable<Record<string, Log[]>>({});
 export const error = writable<string | null>(null);
 export const stoppingProcess = writable<boolean>(false);
 
-// --- Derived ---
+// DERIVED
 export const selectedService = derived(
   [services, selectedServiceId],
   ([$services, $selectedServiceId]) =>
@@ -40,30 +45,12 @@ export const isDisableButton = derived(
   })
 );
 
-export function initLogListener() {
-  EventsOn("service:log", (data: { id: string; logView: Log }) => {
-    serviceLogs.update(curr => {
-      let existing = curr[data.id] ?? [];
-
-      // timer logs are replaced instead of appended. so only the latest countdown is shown
-      if (data.logView.log_type === 'TIMER') {
-        existing = existing.filter(l => l.log_type !== 'TIMER')
-      }
-
-      const updated = [...existing, data.logView]
-
-      return { ...curr, [data.id]: updated };
-    });
-  });
-}
-
-// --- Internal helper ---
+// BACKEND API
 async function refreshService(id: string) {
   const result = await GetById(id);
-  services.update(curr => curr.map(s => s.id === id ? result : s));
+  services.update(currServices => currServices.map(s => s.id === id ? result : s));
 }
 
-// --- Public API ---
 export async function getAllServices() {
   try {
     const result = await GetAll();
@@ -76,6 +63,7 @@ export async function getAllServices() {
 
 export async function startService(id: string) {
   try {
+    await clearLog(id)
     await Start(id);
     await refreshService(id);
     error.set(null);
@@ -118,6 +106,54 @@ export async function restartAllServices() {
   await startAllServices();
 }
 
+// HELPER
+export function initLogListener() {
+  EventsOn("service:log", (data: { id: string; logView: Log }) => {
+    if (data.logView.log_type === 'TIMER') {
+      replaceTimerLog(data.id, data.logView)
+    } else {
+      appendLog(data.id, data.logView)
+    }
+  });
+}
+
+export async function clearLog(id: string) {
+  serviceLogs.update(currLogs => { return { ...currLogs, [id]: [] } })
+}
 
 
+// timer logs are replaced instead of appended. so only the latest countdown is shown
+function replaceTimerLog(id: string, entry: Log) {
+  serviceLogs.update(currLogs => {
+    let selectedLog = currLogs[id] ?? [];
+    let withoutTimer = selectedLog.filter(l => l.log_type !== 'TIMER')
+    const updatedLog = [...withoutTimer, entry]
+    return { ...currLogs, [id]: updatedLog };
+  });
+}
 
+export function appendLog(id: string, entry: Log) {
+  serviceLogs.update((currLogs) => {
+    if (!currLogs[id]) {
+      currLogs[id] = [];
+      logBufferSizeByService[id] = 0;
+    }
+
+    const selectedLog = currLogs[id];
+
+    selectedLog.push(entry);
+    logBufferSizeByService[id] += entry.log.length;
+
+    if (logBufferSizeByService[id] > MAX_BYTES) {
+      const dropCount = Math.floor(selectedLog.length * TRIM_RATIO);
+      const droppedBytes = selectedLog
+        .slice(0, dropCount)
+        .reduce((sum, e) => sum + e.log.length, 0);
+
+      selectedLog.splice(0, dropCount);
+      logBufferSizeByService[id] -= droppedBytes;
+    }
+
+    return currLogs;
+  });
+}
